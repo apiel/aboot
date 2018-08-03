7aa5827f1ac383dd95777b41c1e4328c83bebb81
Alexandre Piel <alexandre.piel@gmail.com>
2018-08-03 22:03:48 +0200

    boot on 0x2000

--- aboot/aboot.c
+++ aboot/aboot.c
@@ -2,126 +2,62 @@
 #include "aboot-private.h"
 #include <aboot-hex2a.h>
 
-void sleep(void) {
-	uint8 loop;
-	for(loop = 2; loop > 0; loop--) {
-		ets_printf("sleep %d\r\n", loop);
-		ets_delay_us(1000000);
-	}
+static uint8 write_conf(void) {
+	uint8 buffer[SECTOR_SIZE];
+	uint8 rom_to_boot;
+
+	aboot_config *romconf = (aboot_config*)buffer;
+
+	// read boot config
+	SPIRead(BOOT_CONFIG_SECTOR * SECTOR_SIZE, buffer, SECTOR_SIZE);
+	rom_to_boot = romconf->current_rom;
+
+	ets_printf("Writing default boot config.\r\n");
+	ets_memset(romconf, 0x00, sizeof(aboot_config));
+	romconf->magic = BOOT_CONFIG_MAGIC;
+	romconf->version = BOOT_CONFIG_VERSION;
+	romconf->count = MAX_ROMS;
+	romconf->roms[0] = ROM_ADDR_1;
+	romconf->roms[1] = ROM_ADDR_2;
+	romconf->current_rom = 0;
+	SPIEraseSector(BOOT_CONFIG_SECTOR);
+	SPIWrite(BOOT_CONFIG_SECTOR * SECTOR_SIZE, buffer, SECTOR_SIZE);
+
+	return rom_to_boot;
 }
 
-static uint32 search(uint32 readpos, uint8 number) {
-	uint32 romaddr = 0;
-	uint8 buffer[2];
-	// for(;readpos < 0x100000;readpos++) {
-	for(;readpos < 0x100000;readpos += SECTOR_SIZE) {
-		if (SPIRead(readpos, buffer, sizeof(buffer)) != 0) {
-			return 0;
-		}
-		if (buffer[0] == ROM_MAGIC_NEW1 && buffer[1] == ROM_MAGIC_NEW2) {
-			ets_printf("Found magic at %d.\r\n", readpos);
-			romaddr = readpos;
-			if (number == 0) {
-				ets_printf("Start here.\r\n");
-				break;
-			} else {
-				number--;
-			}
-		}
-	}
-	return romaddr;
-}
-
-static uint32 check_image(uint32 readpos) {
+static uint32 get_run_address() {
 	uint8 buffer[BUFFER_SIZE];
-	uint8 sectcount;
-	uint8 sectcurrent;
-	uint8 *writepos;
-	uint8 chksum = CHKSUM_INIT;
-	uint32 loop;
-	uint32 remaining;
-	uint32 romaddr;
+	uint32 runaddr;
 
 	rom_header_new *header = (rom_header_new*)buffer;
-	// section_header *section = (section_header*)buffer;
 
-	if (SPIRead(readpos, header, sizeof(rom_header_new)) != 0) {
+	if (SPIRead(ROM_ADDR_1, header, sizeof(rom_header_new)) != 0) {
 		return 0;
 	}
 
 	ets_printf("Header size: %d\r\n", header->len + sizeof(rom_header_new));
 	// new type, has extra header and irom section first
-	romaddr = readpos + header->len + sizeof(rom_header_new);
+	runaddr = ROM_ADDR_1 + header->len + sizeof(rom_header_new);
 
-	return romaddr;
+	return runaddr;
 }
 
-#define ROM_TO_BOOT_ON 1
-
-void write_romconf(uint32 romAddr) {
-	uint8 buffer[SECTOR_SIZE];
-
-	aboot_config *romconf = (aboot_config*)buffer;
-	// rom_header *header = (rom_header*)buffer;
-
-	// read rom header
-	// SPIRead(0, header, sizeof(rom_header));
-	// flashsize = 0x100000;
-	uint32 flashsize = 0x200000;
-
-	// read boot config
-	SPIRead(BOOT_CONFIG_SECTOR * SECTOR_SIZE, buffer, SECTOR_SIZE);
-
-	// create a default config for a standard 2 rom setup
-	ets_printf("Writing default boot config.\r\n");
-	ets_memset(romconf, 0x00, sizeof(aboot_config));
-	romconf->magic = BOOT_CONFIG_MAGIC;
-	romconf->version = BOOT_CONFIG_VERSION;
-	romconf->count = 2;
-	romconf->roms[0] = SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1);
-	romconf->roms[1] = romAddr;
-	romconf->current_rom = ROM_TO_BOOT_ON;
-	// romconf->count = 2;
-	// romconf->roms[0] = SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1);
-	// romconf->roms[1] = (flashsize / 2) + (SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1));
-	// romconf->current_rom = ROM_TO_BOOT_ON;
-	// write new config sector
-	SPIEraseSector(BOOT_CONFIG_SECTOR);
-	SPIWrite(BOOT_CONFIG_SECTOR * SECTOR_SIZE, buffer, SECTOR_SIZE);
-}
-
-// prevent this function being placed inline with main
-// to keep main's stack size as small as possible
-// don't mark as static or it'll be optimised out when
-// using the assembler stub
 uint32 NOINLINE find_image(void) {
-
-	uint8 flag;
 	uint32 runAddr;
-	int32 romToBoot;
-	uint8 updateConfig = FALSE;
+	uint8 rom_to_boot;
 
-	uint32 romAddr = search(SECTOR_SIZE, ROM_TO_BOOT_ON);
-	write_romconf(romAddr);
-	runAddr = check_image(romAddr);
+	rom_to_boot = write_conf();
+	if (rom_to_boot > 0) {
+		ets_printf("We should copy the new Rom from %x to %x.\r\n", ROM_ADDR_2, ROM_ADDR_1);
+	}
 
-	// runAddr = check_image(SECTOR_SIZE * (BOOT_CONFIG_SECTOR + 1));
-	// runAddr = 263712;
-
+	runAddr = get_run_address();
 	ets_printf("Booting at ram %d.\r\n", runAddr);
 	// copy the loader to top of iram
 	ets_memcpy((void*)_text_addr, _text_data, _text_len);
-	// return address to load from
 
-// ABC[233,3].
-// ABC[233,3]. <<<<<
-	// uint8 buffer2[2];
-	// if (SPIRead(runAddr, buffer2, sizeof(buffer2)) == 0) {
-	// 	ets_printf("ABC[%d,%d].\r\n", buffer2[0], buffer2[1]);
-	// }
-	// sleep();
 	return runAddr;
-
 }
 
 #ifdef BOOT_NO_ASM
